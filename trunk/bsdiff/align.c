2817315341caa0d8df2e065309a7bfe08ee83273
cperciva <cperciva@300a1688-e2ff-6a19-dc9b-c3cf27a1c4d7>
2012-11-13 10:06:22 +0000

    Split out alignment-generation code into a separate file.

---
+++ trunk/bsdiff/align.c
@@ -0,0 +1,326 @@
+/*-
+ * Copyright 2003-2005, 2012 Colin Percival
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted providing that the following conditions 
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "alignment.h"
+#include "qsufsort.h"
+
+#include "align.h"
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+static size_t
+matchlen(uint8_t *old, size_t oldsize, uint8_t *new, size_t newsize)
+{
+	size_t i;
+
+	for (i = 0; (i < oldsize) && (i < newsize); i++)
+		if (old[i] != new[i])
+			break;
+
+	return i;
+}
+
+static size_t
+search(size_t *I, uint8_t *old, size_t oldsize, uint8_t *new, size_t newsize,
+    size_t st, size_t en, size_t *pos)
+{
+	size_t x, y;
+
+	if (en - st < 2) {
+		x = matchlen(old + I[st], oldsize - I[st], new, newsize);
+		y = matchlen(old + I[en], oldsize - I[en], new, newsize);
+
+		if (x > y) {
+			*pos = I[st];
+			return x;
+		} else {
+			*pos = I[en];
+			return y;
+		}
+	}
+
+	x = st + (en - st)/2;
+	if (memcmp(old + I[x], new, MIN(oldsize - I[x], newsize)) < 0) {
+		return search(I, old, oldsize, new, newsize, x, en, pos);
+	} else {
+		return search(I, old, oldsize, new, newsize, st, x, pos);
+	}
+}
+
+ALIGNMENT
+align(uint8_t * new, size_t newsize, uint8_t * old, size_t oldsize)
+{
+	size_t *I;
+	ALIGNMENT A;
+	struct alignseg aseg;
+	struct alignseg * asegp, * asegp2;
+	size_t scan, pos, len;
+	size_t lastoffset;
+	size_t oldscore, scsc;
+	size_t alenmax, nposmin;
+	size_t s;
+	size_t i, j, k;
+
+	/* Suffix sort the old file. */
+	if ((I = qsufsort(old, oldsize)) == NULL)
+		err(1, NULL);
+
+	/* Initialize empty alignment array. */
+	if ((A = alignment_init(0)) == NULL)
+		err(1, NULL);
+
+	/* Anchor the alignment at the start of the file. */
+	aseg.alen = aseg.npos = aseg.opos = 0;
+	if (alignment_append(A, &aseg, 1))
+		err(1, NULL);
+	lastoffset = 0;
+
+	/* Scan through new, constructing an alignment against old. */
+	for (scan = 0; scan < newsize; scan += len) {
+		/*
+		 * Look for the next values where new[scan .. scan + len - 1]
+		 * matches old[pos .. pos + len - 1] exactly but they mismatch
+		 * old[scan + lastoffset .. scan + lastoffset + len - 1] in at
+		 * least 8 bytes.
+		 */
+		for (oldscore = 0, scsc = scan; scan < newsize; scan++) {
+			/*
+			 * Find the position in the old string where the string
+			 * new[scan .. newsize - 1] matches best.
+			 */
+			len = search(I, old, oldsize, new + scan, newsize-scan,
+			    0, oldsize, &pos);
+
+			/*
+			 * Increment oldscore for every byte between scsc and
+			 * scan + len which matches with our previous offset.
+			 */
+			for( ; scsc < scan + len; scsc++)
+				if((scsc + lastoffset < oldsize) &&
+				    (old[scsc + lastoffset] == new[scsc]))
+					oldscore++;
+
+			/*
+			 * If the old offset matches for the entire length of
+			 * the alignment and that length is non-zero (i.e.,
+			 * the old offset is one of several optimal alignments
+			 * at this position), continue looking from the end of
+			 * the matched region.
+			 */
+			if ((len == oldscore) && (len != 0))
+				break;
+
+			/*
+			 * If the new offset matches 8 or more characters which
+			 * the previous offset didn't match, record this as a
+			 * new alignment segment.
+			 */
+			if (len > oldscore + 8) {
+				aseg.alen = len;
+				aseg.npos = scan;
+				aseg.opos = pos;
+				if (alignment_append(A, &aseg, 1))
+					err(1, NULL);
+				lastoffset = pos - scan;
+				break;
+			}
+
+			/*
+			 * Decrement oldscore if the byte at position scan
+			 * matches using the old offset.  This maintains the
+			 * invariant that upon entering the loop oldscore is
+			 * equal to the number of bytes in new[scan .. scsc - 1]
+			 * which match using the old offset.
+			 */
+			if ((scan + lastoffset < oldsize) &&
+			    (old[scan + lastoffset] == new[scan]))
+				oldscore--;
+		}
+	}
+
+	/*
+	 * Delete alignments which aren't much better than their successors.
+	 * The selection of segments above (using oldscore) ensures that each
+	 * segment matches at least 8 bytes which don't match at the previous
+	 * offset; now we turn around and apply that requirement in the
+	 * opposite direction.
+	 */
+#if 0
+	for (k = j = 0; j + 1< alignment_getsize(A); j++) {
+		asegp = alignment_get(A, k);
+		asegp2 = alignment_get(A, j + 1);
+
+		/* Always keep the first alignment; it's an anchor. */
+		if (k == 0) {
+			asegp = alignment_get(A, ++k);
+			memcpy(asegp, asegp2, sizeof(*asegp));
+			continue;
+		}
+
+		/* If the new alignment doesn't fit, keep the old one. */
+		if (asegp->npos + asegp2->opos < asegp2->npos) {
+			asegp = alignment_get(A, ++k);
+			memcpy(asegp, asegp2, sizeof(*asegp));
+			continue;
+		}
+
+		/* Count matches within segment k using j's alignment. */
+		lastoffset = asegp2->opos - asegp2->npos;
+		for (s = i = 0; i < asegp->alen; i++) {
+			if (old[asegp->npos + i + lastoffset] ==
+			    new[asegp->npos + i])
+				s++;
+		}
+
+		/* If more than 8 mismatches, keep this alignment segment. */
+		if (s + 8 < asegp->alen) {
+			asegp = alignment_get(A, ++k);
+			memcpy(asegp, asegp2, sizeof(*asegp));
+			continue;
+		}
+
+		/* Replace this segment with an extension of the next one. */
+		asegp->alen = asegp2->npos + asegp2->alen - asegp->npos;
+		asegp->opos = asegp->npos + asegp2->opos - asegp2->npos;
+	}
+	alignment_shrink(A, j - k);
+#endif
+
+	/* Scan through alignments extending them forwards. */
+	for (j = 0; j < alignment_getsize(A); j++) {
+		asegp = alignment_get(A, j);
+
+		/* What's the furthest we could go? */
+		if (j + 1 < alignment_getsize(A))
+			alenmax = alignment_get(A, j + 1)->npos - asegp->npos;
+		else
+			alenmax = newsize - asegp->npos;
+		if (asegp->opos + alenmax > oldsize)
+			alenmax = oldsize - asegp->opos;
+
+		/* Extend as long as we match at least 50%. */
+		s = 0;
+		for (i = asegp->alen; i < alenmax; ) {
+			if (old[asegp->opos + i] == new[asegp->npos + i])
+				s++;
+			i++;
+			if (s * 2 > i - asegp->alen) {
+				s = 0;
+				asegp->alen = i;
+			}
+		}
+	}
+
+	/* Extend alignments backwards, resolving any resulting overlaps. */
+	for (j = 0; j + 1 < alignment_getsize(A); j++) {
+		asegp = alignment_get(A, j);
+		asegp2 = alignment_get(A, j + 1);
+
+		/* How far back can we go? */
+		if (j > 0)
+			nposmin = asegp->npos;
+		else
+			nposmin = 0;
+		if (nposmin + asegp2->opos < asegp2->npos)
+			nposmin = asegp2->npos - asegp2->opos;
+
+		/* Extend as long as we match at least 50%. */
+		s = 0;
+		for (i = asegp2->npos; i > nposmin; ) {
+			if (old[asegp2->opos - asegp2->npos + i - 1] ==
+			    new[i - 1])
+				s++;
+			i--;
+			if (s * 2 + i > asegp2->npos) {
+				asegp2->alen += asegp2->npos - i;
+				asegp2->opos -= asegp2->npos - i;
+				asegp2->npos = i;
+				s = 0;
+			}
+		}
+
+		/* If the extended alignments don't overlap, move on. */
+		if (asegp->npos + asegp->alen <= asegp2->npos)
+			continue;
+
+		/* Find the optimal position to split. */
+		s = 0;
+		for (i = asegp->npos + asegp->alen; i > asegp2->npos; ) {
+			if (old[i - 1 + (asegp->opos - asegp->npos)] != new[i - 1])
+				s++;
+			if (old[i - 1 + (asegp2->opos - asegp2->npos)] == new[i - 1])
+				s++;
+			i--;
+			if (i + s >= asegp->npos + asegp->alen) {
+				asegp->alen = i - asegp->npos;
+				s = 0;
+			}
+		}
+		asegp2->alen -= (asegp->npos + asegp->alen) - asegp2->npos;
+		asegp2->opos += (asegp->npos + asegp->alen) - asegp2->npos;
+		asegp2->npos = asegp->npos + asegp->alen;
+	}
+
+	/* Delete any alignment segments which are now empty. */
+	for (k = j = 1; j + 1< alignment_getsize(A); j++) {
+		asegp = alignment_get(A, k);
+		asegp2 = alignment_get(A, j + 1);
+
+		/* Keep if non-empty. */
+		if (asegp->alen)
+			k++;
+
+		/* Copy next segment into place. */
+		memcpy(alignment_get(A, k), asegp2, sizeof(*asegp));
+	}
+	alignment_shrink(A, j - k);
+
+	/*
+	 * Push a final segment onto our alignment array to ensure we get any
+	 * trailing copied bytes.  We don't need a trailing seek, so set opos
+	 * equal to where the old file pointer will be already.
+	 */
+	asegp = alignment_get(A, alignment_getsize(A) - 1);
+	aseg.alen = 0;
+	aseg.npos = newsize;
+	aseg.opos = asegp->opos + asegp->alen;
+	if (alignment_append(A, &aseg, 1))
+		err(1, NULL);
+
+	/* Free the suffix array. */
+	free(I);
+
+	/* Success! */
+	return (A);
+
+	/* Failure! */
+	return (NULL);
+}
--- trunk/bsdiff/bsdiff.c
+++ trunk/bsdiff/bsdiff.c
@@ -32,54 +32,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "align.h"
 #include "alignment.h"
 #include "mapfile.h"
-#include "qsufsort.h"
 
 #ifndef O_BINARY
 #define O_BINARY 0
 #endif
-
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
-static size_t
-matchlen(uint8_t *old, size_t oldsize, uint8_t *new, size_t newsize)
-{
-	size_t i;
-
-	for (i = 0; (i < oldsize) && (i < newsize); i++)
-		if (old[i] != new[i])
-			break;
-
-	return i;
-}
-
-static size_t
-search(size_t *I, uint8_t *old, size_t oldsize, uint8_t *new, size_t newsize,
-    size_t st, size_t en, size_t *pos)
-{
-	size_t x, y;
-
-	if (en - st < 2) {
-		x = matchlen(old + I[st], oldsize - I[st], new, newsize);
-		y = matchlen(old + I[en], oldsize - I[en], new, newsize);
-
-		if (x > y) {
-			*pos = I[st];
-			return x;
-		} else {
-			*pos = I[en];
-			return y;
-		}
-	}
-
-	x = st + (en - st)/2;
-	if (memcmp(old + I[x], new, MIN(oldsize - I[x], newsize)) < 0) {
-		return search(I, old, oldsize, new, newsize, x, en, pos);
-	} else {
-		return search(I, old, oldsize, new, newsize, st, x, pos);
-	}
-}
 
 static void
 offtout(int64_t x, uint8_t *buf)
@@ -110,16 +69,11 @@ main(int argc, char *argv[])
 	int oldfd, newfd;
 	uint8_t *old, *new;
 	size_t oldsize, newsize;
-	size_t *I;
 	ALIGNMENT A;
-	struct alignseg aseg;
 	struct alignseg * asegp, * asegp2;
-	size_t scan, pos, len;
+	size_t len;
 	size_t lastscan, lastpos, lastoffset;
-	size_t oldscore, scsc;
-	size_t alenmax, nposmin;
-	size_t s;
-	size_t i, j, k;
+	size_t i, j;
 	size_t dblen, eblen;
 	uint8_t *db = NULL, *eb = NULL;
 	uint8_t buf[24];
@@ -135,235 +89,13 @@ main(int argc, char *argv[])
 	if ((old = mapfile(argv[1], &oldfd, &oldsize)) == NULL)
 		err(1, "Cannot map file: %s", argv[1]);
 
-	/* Suffix sort the old file. */
-	if ((I = qsufsort(old, oldsize)) == NULL)
-		err(1, NULL);
-
 	/* Map the new file into memory. */
 	if ((new = mapfile(argv[2], &newfd, &newsize)) == NULL)
 		err(1, "Cannot map file: %s", argv[2]);
 
-	/* Initialize empty alignment array. */
-	if ((A = alignment_init(0)) == NULL)
-		err(1, NULL);
-
-	/* Anchor the alignment at the start of the file. */
-	aseg.alen = aseg.npos = aseg.opos = 0;
-	if (alignment_append(A, &aseg, 1))
-		err(1, NULL);
-	lastoffset = 0;
-
-	/* Scan through new, constructing an alignment against old. */
-	for (scan = 0; scan < newsize; scan += len) {
-		/*
-		 * Look for the next values where new[scan .. scan + len - 1]
-		 * matches old[pos .. pos + len - 1] exactly but they mismatch
-		 * old[scan + lastoffset .. scan + lastoffset + len - 1] in at
-		 * least 8 bytes.
-		 */
-		for (oldscore = 0, scsc = scan; scan < newsize; scan++) {
-			/*
-			 * Find the position in the old string where the string
-			 * new[scan .. newsize - 1] matches best.
-			 */
-			len = search(I, old, oldsize, new + scan, newsize-scan,
-			    0, oldsize, &pos);
-
-			/*
-			 * Increment oldscore for every byte between scsc and
-			 * scan + len which matches with our previous offset.
-			 */
-			for( ; scsc < scan + len; scsc++)
-				if((scsc + lastoffset < oldsize) &&
-				    (old[scsc + lastoffset] == new[scsc]))
-					oldscore++;
-
-			/*
-			 * If the old offset matches for the entire length of
-			 * the alignment and that length is non-zero (i.e.,
-			 * the old offset is one of several optimal alignments
-			 * at this position), continue looking from the end of
-			 * the matched region.
-			 */
-			if ((len == oldscore) && (len != 0))
-				break;
-
-			/*
-			 * If the new offset matches 8 or more characters which
-			 * the previous offset didn't match, record this as a
-			 * new alignment segment.
-			 */
-			if (len > oldscore + 8) {
-				aseg.alen = len;
-				aseg.npos = scan;
-				aseg.opos = pos;
-				if (alignment_append(A, &aseg, 1))
-					err(1, NULL);
-				lastoffset = pos - scan;
-				break;
-			}
-
-			/*
-			 * Decrement oldscore if the byte at position scan
-			 * matches using the old offset.  This maintains the
-			 * invariant that upon entering the loop oldscore is
-			 * equal to the number of bytes in new[scan .. scsc - 1]
-			 * which match using the old offset.
-			 */
-			if ((scan + lastoffset < oldsize) &&
-			    (old[scan + lastoffset] == new[scan]))
-				oldscore--;
-		}
-	}
-
-	/*
-	 * Delete alignments which aren't much better than their successors.
-	 * The selection of segments above (using oldscore) ensures that each
-	 * segment matches at least 8 bytes which don't match at the previous
-	 * offset; now we turn around and apply that requirement in the
-	 * opposite direction.
-	 */
-#if 0
-	for (k = j = 0; j + 1< alignment_getsize(A); j++) {
-		asegp = alignment_get(A, k);
-		asegp2 = alignment_get(A, j + 1);
-
-		/* Always keep the first alignment; it's an anchor. */
-		if (k == 0) {
-			asegp = alignment_get(A, ++k);
-			memcpy(asegp, asegp2, sizeof(*asegp));
-			continue;
-		}
-
-		/* If the new alignment doesn't fit, keep the old one. */
-		if (asegp->npos + asegp2->opos < asegp2->npos) {
-			asegp = alignment_get(A, ++k);
-			memcpy(asegp, asegp2, sizeof(*asegp));
-			continue;
-		}
-
-		/* Count matches within segment k using j's alignment. */
-		lastoffset = asegp2->opos - asegp2->npos;
-		for (s = i = 0; i < asegp->alen; i++) {
-			if (old[asegp->npos + i + lastoffset] ==
-			    new[asegp->npos + i])
-				s++;
-		}
-
-		/* If more than 8 mismatches, keep this alignment segment. */
-		if (s + 8 < asegp->alen) {
-			asegp = alignment_get(A, ++k);
-			memcpy(asegp, asegp2, sizeof(*asegp));
-			continue;
-		}
-
-		/* Replace this segment with an extension of the next one. */
-		asegp->alen = asegp2->npos + asegp2->alen - asegp->npos;
-		asegp->opos = asegp->npos + asegp2->opos - asegp2->npos;
-	}
-	alignment_shrink(A, j - k);
-#endif
-
-	/* Scan through alignments extending them forwards. */
-	for (j = 0; j < alignment_getsize(A); j++) {
-		asegp = alignment_get(A, j);
-
-		/* What's the furthest we could go? */
-		if (j + 1 < alignment_getsize(A))
-			alenmax = alignment_get(A, j + 1)->npos - asegp->npos;
-		else
-			alenmax = newsize - asegp->npos;
-		if (asegp->opos + alenmax > oldsize)
-			alenmax = oldsize - asegp->opos;
-
-		/* Extend as long as we match at least 50%. */
-		s = 0;
-		for (i = asegp->alen; i < alenmax; ) {
-			if (old[asegp->opos + i] == new[asegp->npos + i])
-				s++;
-			i++;
-			if (s * 2 > i - asegp->alen) {
-				s = 0;
-				asegp->alen = i;
-			}
-		}
-	}
-
-	/* Extend alignments backwards, resolving any resulting overlaps. */
-	for (j = 0; j + 1 < alignment_getsize(A); j++) {
-		asegp = alignment_get(A, j);
-		asegp2 = alignment_get(A, j + 1);
-
-		/* How far back can we go? */
-		if (j > 0)
-			nposmin = asegp->npos;
-		else
-			nposmin = 0;
-		if (nposmin + asegp2->opos < asegp2->npos)
-			nposmin = asegp2->npos - asegp2->opos;
-
-		/* Extend as long as we match at least 50%. */
-		s = 0;
-		for (i = asegp2->npos; i > nposmin; ) {
-			if (old[asegp2->opos - asegp2->npos + i - 1] ==
-			    new[i - 1])
-				s++;
-			i--;
-			if (s * 2 + i > asegp2->npos) {
-				asegp2->alen += asegp2->npos - i;
-				asegp2->opos -= asegp2->npos - i;
-				asegp2->npos = i;
-				s = 0;
-			}
-		}
-
-		/* If the extended alignments don't overlap, move on. */
-		if (asegp->npos + asegp->alen <= asegp2->npos)
-			continue;
-
-		/* Find the optimal position to split. */
-		s = 0;
-		for (i = asegp->npos + asegp->alen; i > asegp2->npos; ) {
-			if (old[i - 1 + (asegp->opos - asegp->npos)] != new[i - 1])
-				s++;
-			if (old[i - 1 + (asegp2->opos - asegp2->npos)] == new[i - 1])
-				s++;
-			i--;
-			if (i + s >= asegp->npos + asegp->alen) {
-				asegp->alen = i - asegp->npos;
-				s = 0;
-			}
-		}
-		asegp2->alen -= (asegp->npos + asegp->alen) - asegp2->npos;
-		asegp2->opos += (asegp->npos + asegp->alen) - asegp2->npos;
-		asegp2->npos = asegp->npos + asegp->alen;
-	}
-
-	/* Delete any alignment segments which are now empty. */
-	for (k = j = 1; j + 1< alignment_getsize(A); j++) {
-		asegp = alignment_get(A, k);
-		asegp2 = alignment_get(A, j + 1);
-
-		/* Keep if non-empty. */
-		if (asegp->alen)
-			k++;
-
-		/* Copy next segment into place. */
-		memcpy(alignment_get(A, k), asegp2, sizeof(*asegp));
-	}
-	alignment_shrink(A, j - k);
-
-	/*
-	 * Push a final segment onto our alignment array to ensure we get any
-	 * trailing copied bytes.  We don't need a trailing seek, so set opos
-	 * equal to where the old file pointer will be already.
-	 */
-	asegp = alignment_get(A, alignment_getsize(A) - 1);
-	aseg.alen = 0;
-	aseg.npos = newsize;
-	aseg.opos = asegp->opos + asegp->alen;
-	if (alignment_append(A, &aseg, 1))
-		err(1, NULL);
+	/* Compute an alignment of the two files. */
+	if ((A = align(new, newsize, old, oldsize)) == NULL)
+		err(1, "Error aligning files");
 
 	/* Create the patch file */
 	if ((pf = fopen(argv[3], "wb")) == NULL)
@@ -501,7 +233,6 @@ main(int argc, char *argv[])
 	alignment_free(A);
 	free(db);
 	free(eb);
-	free(I);
 
 	/* Release memory mappings. */
 	unmapfile(new, newfd, newsize);
