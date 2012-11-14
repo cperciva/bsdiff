792e94f1bca8d6925a2f818ae549807c35e03428
cperciva <cperciva@300a1688-e2ff-6a19-dc9b-c3cf27a1c4d7>
2012-11-14 17:48:42 +0000

    Refactor, and in the process switch from constructing the entire diff and extra blocks at once to feeding them into the bzip2 compressor as we move through the alignment.

--- trunk/bsdiff/writepatch.c
+++ trunk/bsdiff/writepatch.c
@@ -31,6 +31,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "warnp.h"
+
 #include "alignment.h"
 
 #include "writepatch.h"
@@ -58,6 +60,242 @@ offtout(int64_t x, uint8_t *buf)
 		buf[7] |= 0x80;
 }
 
+/* Write an encoded int64_t to the bz2 stream. */
+static int
+writeval(BZFILE * fbz2, int64_t val)
+{
+	uint8_t buf[8];
+	int bz2err;
+
+	/* Expand the value into a buffer. */
+	offtout(val, buf);
+
+	/* Write it out. */
+	BZ2_bzWrite(&bz2err, fbz2, buf, 8);
+
+	/* Check error status, warn, and return. */
+	if (bz2err != BZ_OK) {
+		warn0("BZ2_bzWrite failed: %d", bz2err);
+		return (-1);
+	} else
+		return (0);
+}
+
+/* Append the (compressed) control block to the the file. */
+static int
+writectrl(FILE * f, ALIGNMENT A, size_t newsize)
+{
+	struct alignseg * asegp;
+	BZFILE * pfbz2;
+	size_t opos, npos;
+	size_t i;
+	int bz2err;
+
+	/* Start writing control tuples. */
+	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, f, 9, 0, 0)) == NULL) {
+		warn0("BZ2_bzWriteOpen failed: %d", bz2err);
+		goto err0;
+	}
+
+	/*
+	 * The control block starts with "copy X bytes from position 0 in the
+	 * old file to position 0 in the new file".  If we have a first
+	 * alignment segment and it aligns position 0 to position 0, this is
+	 * great -- we can emit the number of bytes to copy and move on to the
+	 * next alignment segment.  Otherwise, we need to say "copy zero bytes"
+	 * and start processing at the start of the alignment list.
+	 */
+	if ((alignment_getsize(A) > 0) &&
+	    (alignment_get(A, 0)->npos == 0) &&
+	    (alignment_get(A, 0)->opos == 0)) {
+		asegp = alignment_get(A, 0);
+		if (writeval(pfbz2, asegp->alen))
+			goto err1;
+		npos = opos = asegp->alen;
+		i = 1;
+	} else {
+		if (writeval(pfbz2, 0))
+			goto err1;
+		npos = opos = 0;
+		i = 0;
+	}
+
+	/* Read through the alignment array, processing segments. */
+	for ( ; i < alignment_getsize(A); i++) {
+		asegp = alignment_get(A, i);
+
+		/* Extra length is the gap before this segment starts. */
+		if (writeval(pfbz2, asegp->npos - npos))
+			goto err1;
+
+		/* Seek length is the difference between positions in old. */
+		if (writeval(pfbz2, asegp->opos - opos))
+			goto err1;
+
+		/* Diff length is the length of the aligned region. */
+		if (writeval(pfbz2, asegp->alen))
+			goto err1;
+
+		/* Update our pointers within the two files. */
+		npos = asegp->npos + asegp->alen;
+		opos = asegp->opos + asegp->alen;
+	}
+
+	/* Extra length is the rest up to the end of the file. */
+	if (writeval(pfbz2, newsize - npos))
+		goto err1;
+
+	/* Seek length is zero; no point seeking after we're finished. */
+	if (writeval(pfbz2, 0))
+		goto err1;
+
+	/* We've finished writing control tuples. */
+	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+	if (bz2err != BZ_OK) {
+		warn0("BZ2_bzWriteClose failed: %d", bz2err);
+		goto err0;
+	}
+
+	/* Success! */
+	return (0);
+
+err1:
+	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+err0:
+	/* Failure! */
+	return (-1);
+}
+
+/* Write a segment of diff. */
+static int
+writediffseg(BZFILE * fbz2, const uint8_t * new, const uint8_t * old,
+    size_t len)
+{
+	uint8_t buf[4096];
+	size_t i;
+	int bz2err;
+
+	/* Loop until we've written everything. */
+	while (len > 0) {
+		/* Diff until we fill the buffer or run out of data. */
+		for (i = 0; i < 4096 && len > 0; i++, len--)
+			buf[i] = *new++ - *old++;
+
+		/* Write out the diffed data. */
+		BZ2_bzWrite(&bz2err, fbz2, buf, i);
+		if (bz2err != BZ_OK) {
+			warn0("BZ2_bzWrite failed: %d", bz2err);
+			goto err0;
+		}
+	}
+
+	/* Success! */
+	return (0);
+
+err0:
+	/* Failure! */
+	return (-1);
+}
+
+/* Append the (compressed) diff block to the the file. */
+static int
+writediff(FILE * f, ALIGNMENT A, const uint8_t * new, const uint8_t * old)
+{
+	struct alignseg * asegp;
+	BZFILE * pfbz2;
+	size_t i;
+	int bz2err;
+
+	/* Start writing diff segments. */
+	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, f, 9, 0, 0)) == NULL) {
+		warn0("BZ2_bzWriteOpen failed: %d", bz2err);
+		goto err0;
+	}
+
+	/* Write segments one by one. */
+	for (i = 0; i < alignment_getsize(A); i++) {
+		asegp = alignment_get(A, i);
+
+		if (writediffseg(pfbz2, &new[asegp->npos], &old[asegp->opos],
+		    asegp->alen))
+			goto err1;
+	}
+
+	/* We've finished writing the diff block. */
+	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+	if (bz2err != BZ_OK) {
+		warn0("BZ2_bzWriteClose failed: %d", bz2err);
+		goto err0;
+	}
+
+	/* Success! */
+	return (0);
+
+err1:
+	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
+err0:
+	/* Failure! */
+	return (-1);
+}
+
+/* Append the (compressed) extra block to the file. */
+static int
+writeextra(FILE * f, ALIGNMENT A, const uint8_t * new, size_t newsize)
+{
+	struct alignseg * asegp;
+	BZFILE * fbz2;
+	size_t npos;
+	size_t i;
+	int bz2err;
+
+	/* Start writing extra segments. */
+	if ((fbz2 = BZ2_bzWriteOpen(&bz2err, f, 9, 0, 0)) == NULL) {
+		warn0("BZ2_bzWriteOpen failed: %d", bz2err);
+		goto err0;
+	}
+
+	/* Write segments one by one. */
+	npos = 0;
+	for (i = 0; i < alignment_getsize(A); i++) {
+		asegp = alignment_get(A, i);
+
+		/* Write data up to the start of the next aligned section. */
+		BZ2_bzWrite(&bz2err, fbz2, (void *)(uintptr_t)&new[npos],
+		    asegp->npos - npos);
+		if (bz2err != BZ_OK) {
+			warn0("BZ2_bzWrite failed: %d", bz2err);
+			goto err1;
+		}
+
+		/* The next unaligned section starts here. */
+		npos = asegp->npos + asegp->alen;
+	}
+
+	/* Write extra data from the end of the last aligned section to EOF. */
+	BZ2_bzWrite(&bz2err, fbz2, (void *)(uintptr_t)&new[npos],
+	    newsize - npos);
+	if (bz2err != BZ_OK) {
+		warn0("BZ2_bzWrite failed: %d", bz2err);
+		goto err1;
+	}
+
+	/* We've finished writing the extra block. */
+	BZ2_bzWriteClose(&bz2err, fbz2, 0, NULL, NULL);
+	if (bz2err != BZ_OK) {
+		warn0("BZ2_bzWriteClose failed: %d", bz2err);
+		goto err0;
+	}
+
+	/* Success! */
+	return (0);
+
+err1:
+	BZ2_bzWriteClose(&bz2err, fbz2, 0, NULL, NULL);
+err0:
+	/* Failure! */
+	return (-1);
+}
+
 /**
  * writepatch(name, A, new, newsize, old):
  * Write a patch with the specified name based on the alignment A of the new
@@ -67,17 +305,10 @@ void
 writepatch(const char * name, ALIGNMENT A, const uint8_t * new, size_t newsize,
     const uint8_t * old)
 {
-	struct alignseg * asegp, * asegp2;
 	size_t len;
-	size_t lastscan, lastpos, lastoffset;
-	size_t i, j;
-	size_t dblen, eblen;
-	uint8_t *db = NULL, *eb = NULL;
-	uint8_t buf[24];
+	size_t flen;
 	uint8_t header[32];
 	FILE * pf;
-	BZFILE * pfbz2;
-	int bz2err;
 
 	/* Open the patch file for writing. */
 	if ((pf = fopen(name, "wb")) == NULL)
@@ -100,108 +331,27 @@ writepatch(const char * name, ALIGNMENT A, const uint8_t * new, size_t newsize,
 	if (fwrite(header, 32, 1, pf) != 1)
 		err(1, "fwrite(%s)", name);
 
-	/* Start writing control tuples. */
-	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, pf, 9, 0, 0)) == NULL)
-		errx(1, "BZ2_bzWriteOpen, bz2err = %d", bz2err);
-
-	/* Read through the alignment array, writing out control tuples. */
-	lastscan = 0;
-	lastpos = 0;
-	lastoffset = 0;
-	for (i = 0; i + 1 < alignment_getsize(A); i++) {
-		asegp = alignment_get(A, i);
-		asegp2 = alignment_get(A, i + 1);
-
-		/* Diff length is the length of the aligned region. */
-		offtout(asegp->alen, &buf[0]);
-
-		/* Extra length is the gap between this and the next region. */
-		offtout(asegp2->npos - (asegp->npos + asegp->alen), &buf[8]);
-
-		/* Seek length is the difference between positions in old. */
-		offtout(asegp2->opos - (asegp->opos + asegp->alen), &buf[16]);
-
-		BZ2_bzWrite(&bz2err, pfbz2, buf, 24);
-		if (bz2err != BZ_OK)
-			errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
-	}
-
-	/* We've finished writing control tuples. */
-	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
-	if (bz2err != BZ_OK)
-		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
+	/* Write control block. */
+	if (writectrl(pf, A, newsize))
+		exit(1);
 
 	/* Compute size of compressed ctrl data */
 	if ((len = ftello(pf)) == (size_t)(-1))
 		err(1, "ftello");
 	offtout(len - 32, header + 8);
 
-	/* Construct diff block. */
-	for (dblen = i = 0; i + 1 < alignment_getsize(A); i++) {
-		asegp = alignment_get(A, i);
-		dblen += asegp->alen;
-	}
-	if (dblen > 0) {
-		/* Allocate memory for the block. */
-		if ((db = malloc(dblen)) == NULL)
-			err(1, NULL);
-
-		/* Construct it one segment at a time. */
-		for (dblen = i = 0; i + 1 < alignment_getsize(A); i++) {
-			asegp = alignment_get(A, i);
-			for (j = 0; j < asegp->alen; j++)
-				db[dblen + j] = new[asegp->npos + j] -
-				    old[asegp->opos + j];
-			dblen += asegp->alen;
-		}
-	}
-
-	/* Write compressed diff data */
-	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, pf, 9, 0, 0)) == NULL)
-		errx(1, "BZ2_bzWriteOpen, bz2err = %d", bz2err);
-	if (dblen > 0)
-		BZ2_bzWrite(&bz2err, pfbz2, db, dblen);
-	if (bz2err != BZ_OK)
-		errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
-	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
-	if (bz2err != BZ_OK)
-		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
+	/* Write diff block. */
+	if (writediff(pf, A, new, old))
+		exit(1);
 
 	/* Compute size of compressed diff data */
-	if ((newsize = ftello(pf)) == (size_t)(-1))
+	if ((flen = ftello(pf)) == (size_t)(-1))
 		err(1, "ftello");
-	offtout(newsize - len, header + 16);
+	offtout(flen - len, header + 16);
 
-	/* Construct extra block. */
-	for (eblen = i = 0; i + 1 < alignment_getsize(A); i++) {
-		asegp = alignment_get(A, i);
-		asegp2 = alignment_get(A, i + 1);
-		eblen += asegp2->npos - (asegp->npos + asegp->alen);
-	}
-	if (eblen > 0) {
-		/* Allocate memory for the block. */
-		if ((eb = malloc(eblen)) == NULL)
-			err(1, NULL);
-
-		/* Construct it one segment at a time. */
-		for (eblen = i = 0; i + 1 < alignment_getsize(A); i++) {
-			asegp = alignment_get(A, i);
-			asegp2 = alignment_get(A, i + 1);
-			for (j = asegp->npos + asegp->alen; j < asegp2->npos; )
-				eb[eblen++] = new[j++];
-		}
-	}
-
-	/* Write compressed extra data */
-	if ((pfbz2 = BZ2_bzWriteOpen(&bz2err, pf, 9, 0, 0)) == NULL)
-		errx(1, "BZ2_bzWriteOpen, bz2err = %d", bz2err);
-	if (eblen > 0)
-		BZ2_bzWrite(&bz2err, pfbz2, eb, eblen);
-	if (bz2err != BZ_OK)
-		errx(1, "BZ2_bzWrite, bz2err = %d", bz2err);
-	BZ2_bzWriteClose(&bz2err, pfbz2, 0, NULL, NULL);
-	if (bz2err != BZ_OK)
-		errx(1, "BZ2_bzWriteClose, bz2err = %d", bz2err);
+	/* Write extra block. */
+	if (writeextra(pf, A, new, newsize))
+		exit(1);
 
 	/* Seek to the beginning, write the header, and close the file */
 	if (fseeko(pf, 0, SEEK_SET))
@@ -210,8 +360,4 @@ writepatch(const char * name, ALIGNMENT A, const uint8_t * new, size_t newsize,
 		err(1, "fwrite(%s)", name);
 	if (fclose(pf))
 		err(1, "fclose");
-
-	/* Free the memory we used */
-	free(db);
-	free(eb);
 }
